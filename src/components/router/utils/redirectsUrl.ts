c856d68dccb74c0ce74bf781e489939ed18d3596
Kotec <vivalastelmah@gmail.com>
2023-08-23 22:30:01 +0300

    feat: add state class. put state managment logic from app to state class

---
+++ src/components/router/utils/redirectsUrl.ts
@@ -0,0 +1,6 @@
+import PAGES from './pages';
+
+const linksForSignedIn = [PAGES.PROFILE, PAGES.LOG_OUT];
+const linksForSignedOut = [PAGES.LOG_IN, PAGES.SIGN_UP];
+
+export { linksForSignedIn, linksForSignedOut };
