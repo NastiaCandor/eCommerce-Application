f9d5d1bff88434e405c5906bce0bde6efa7b7ff6
ElenaAnisimova <anilena232@mail.ru>
2023-09-09 16:18:31 +0800

    feat: add main page content

--- src/components/view/pages/about/AboutView.ts
+++ src/components/view/pages/about/AboutView.ts
@@ -1,6 +1,9 @@
 import ElementCreator from '../../../utils/ElementCreator';
 import View from '../../View';
 import aboutParams from './about-params';
+import '../../../../assets/img/girl.png';
+import '../../../../assets/img/The_Gershwin_Song.jpg';
+import '../../../../assets/img/vinyl.png';
 
 export default class AboutView extends View {
   constructor() {
@@ -17,6 +20,48 @@ export default class AboutView extends View {
   }
 
   private renderInnerWrapper(): void {
-    this.addInnerElement(new ElementCreator(aboutParams.wrapper));
+    const sloganImgWrapper = new ElementCreator(aboutParams.sloganImgWrapper);
+
+    const imgWrapper = new ElementCreator(aboutParams.imgWrapper);
+    sloganImgWrapper.addInnerElement(imgWrapper);
+
+    const sloganWrapper = new ElementCreator(aboutParams.sloganWrapper);
+    sloganImgWrapper.addInnerElement(sloganWrapper);
+
+    const sloganHeading = new ElementCreator(aboutParams.sloganHeading);
+    sloganWrapper.addInnerElement(sloganHeading);
+
+    const shopInfo = new ElementCreator(aboutParams.infoText);
+    shopInfo.getElement().innerHTML = aboutParams.infoText.textContent;
+
+    const shopInfo2 = new ElementCreator(aboutParams.infoText2);
+    shopInfo2.getElement().innerHTML = aboutParams.infoText2.textContent;
+    sloganWrapper.addInnerElement([shopInfo, shopInfo2]);
+
+    sloganImgWrapper.addInnerElement([sloganWrapper, imgWrapper]);
+
+    const mainImg = new ElementCreator(aboutParams.img);
+    imgWrapper.addInnerElement(mainImg);
+
+    const JazzWrapper = new ElementCreator(aboutParams.innerWrapper);
+    this.addInnerElement(JazzWrapper);
+
+    const sloganWrapper2 = new ElementCreator(aboutParams.sloganWrapper);
+    const sloganHeading2 = new ElementCreator(aboutParams.sloganHeading2);
+    const shopInfoJazz = new ElementCreator(aboutParams.infoTextJazz);
+    const shopInfoJazz2 = new ElementCreator(aboutParams.infoTextJazz2);
+    sloganWrapper2.addInnerElement([sloganHeading2, shopInfoJazz, shopInfoJazz2]);
+
+    const imgWrapperJazz = new ElementCreator(aboutParams.imgWrapperJazz);
+    const jazzImg = new ElementCreator(aboutParams.imgJazz);
+
+    const vinylImg = new ElementCreator(aboutParams.imgVinyl);
+
+    imgWrapperJazz.addInnerElement([vinylImg, jazzImg]);
+
+    // shopInfo.getElement().innerHTML = aboutParams.infoText.textContent;
+
+    JazzWrapper.addInnerElement([imgWrapperJazz, sloganWrapper2]);
+    this.addInnerElement([sloganImgWrapper, JazzWrapper]);
   }
 }
--- src/components/view/pages/about/about-params.ts
+++ src/components/view/pages/about/about-params.ts
@@ -2,11 +2,95 @@ const aboutParams = {
   section: {
     tag: 'section',
   },
-  wrapper: {
+  sloganImgWrapper: {
     tag: 'div',
-    cssClasses: ['about__wrapper', 'wrapper'],
+    cssClasses: ['main-slogan-img__wrapper'],
+  },
+
+  imgWrapper: {
+    tag: 'div',
+    cssClasses: ['main-img__wrapper'],
+  },
+
+  sloganWrapper: {
+    tag: 'div',
+    cssClasses: ['main-slogan__wrapper'],
+  },
+
+  sloganHeading: {
+    tag: 'h2',
+    cssClasses: ['main-slogan__heading'],
+    textContent: 'Discover the Warmth of Vinyl',
+  },
+
+  infoText: {
+    tag: 'p',
+    cssClasses: ['main-slogan__info-text'],
     textContent:
-      'THIS IS MAIN. WHILE ONGOING CROSS-CHECK USER ICONS WILL BE SHOWN HERE FOR A BETTER TESTING EXPIRIENCE',
+      'Vinyl Vibe is a retro  <b>vinyl records</b> shop that offers a unique experience for all music and vintage enthusiasts.',
+  },
+
+  infoText2: {
+    tag: 'p',
+    cssClasses: ['main-slogan__info-text', 'info-text2'],
+    textContent:
+      'Our shop specializes in selling <b>original and rare</b> vinyl records, allowing you to experience the authenticity and warmth of sound that only vinyl can provide.',
+  },
+
+  infoTextElla: {
+    tag: 'p',
+    cssClasses: ['main-slogan__info-text', 'info-text2'],
+    textContent: '“I sing like I feel.” - Ella Fitzgerald​​​​​​​​.',
+  },
+
+  infoTextJazz: {
+    tag: 'p',
+    cssClasses: ['info-text-jazz', 'info-text-jazz-top'],
+    textContent: 'Ella Fitzgerald and More:',
+  },
+
+  infoTextJazz2: {
+    tag: 'a',
+    href: '#',
+    cssClasses: ['info-text-jazz', 'info-text-jazz-bottom'],
+    textContent: 'Jazz Vinyl Records Out Now!',
+  },
+
+  img: {
+    tag: 'img',
+    cssClasses: ['main-img'],
+    alt: 'girl-vinyl',
+    src: '../assets/img/girl.png',
+  },
+
+  innerWrapper: {
+    tag: 'div',
+    cssClasses: ['main-inner__wrapper'],
+  },
+
+  sloganHeading2: {
+    tag: 'h2',
+    cssClasses: ['main-slogan__heading2'],
+    textContent: 'Jazz up Your Vinyl Collection',
+  },
+
+  imgWrapperJazz: {
+    tag: 'div',
+    cssClasses: ['main-img__wrapper', 'jazz-img__wrapper'],
+  },
+
+  imgJazz: {
+    tag: 'img',
+    cssClasses: ['jazz-img'],
+    alt: 'ella-Fitzgerald',
+    src: '../assets/img/The_Gershwin_Song.jpg',
+  },
+
+  imgVinyl: {
+    tag: 'img',
+    cssClasses: ['vinyl-img'],
+    alt: 'vinyl-record',
+    src: '../assets/img/vinyl.png',
   },
 };
 
