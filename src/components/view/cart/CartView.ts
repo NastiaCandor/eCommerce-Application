90b3a9a63a80ef5cf2a0b332f4acc856086a16d4
ElenaAnisimova <anilena232@mail.ru>
2023-09-11 20:05:55 +0800

    feat: add shopping cart basic layout

--- src/components/view/cart/CartView.ts
+++ src/components/view/cart/CartView.ts
@@ -17,6 +17,115 @@ export default class CartView extends View {
   }
 
   private renderInnerWrapper(): void {
-    this.addInnerElement(new ElementCreator(cartParams.wrapper));
+    const cartWrapper = new ElementCreator(cartParams.wrapper);
+    this.addInnerElement(cartWrapper);
+
+    const topWrapper = new ElementCreator(cartParams.topWrapper);
+    const cartHeading = new ElementCreator(cartParams.heading);
+    const clearCartBtn = new ElementCreator(cartParams.clearCartBtn);
+    clearCartBtn.addInnerElement(new ElementCreator(cartParams.clearCartBtnImg));
+    topWrapper.addInnerElement([cartHeading, clearCartBtn]);
+
+    cartWrapper.addInnerElement([topWrapper, this.createCartItemsWrapper()]);
+    const cartAsideMenu = this.assembleAside();
+    this.addInnerElement(cartAsideMenu);
   }
+
+  private createCartItemsWrapper(): ElementCreator {
+    const cartItemsWrapper = new ElementCreator(cartParams.itemsWrapper);
+    cartItemsWrapper.addInnerElement(this.assembleCartItem('src', 'alt', 'Sample Title', '20', '80'));
+    return cartItemsWrapper;
+  }
+
+  private assembleCartItem(src: string, alt: string, title: string, price: string, totalPrice: string): ElementCreator {
+    const cartItem = new ElementCreator(cartParams.cartItem);
+    // itemImage.setImageLink(data[0].url, name['en-US']);
+    const itemImage = new ElementCreator(cartParams.img);
+    itemImage.setImageLink(src, alt);
+
+    const itemName = new ElementCreator(cartParams.title);
+    itemName.setTextContent(title);
+
+    const counterWrapper = new ElementCreator(cartParams.counterWrapper);
+    const minusBtn = new ElementCreator(cartParams.counterBtnMinus);
+    const counterInput = new ElementCreator(cartParams.counterInput);
+    counterInput.setAttribute('type', cartParams.counterInput.type);
+    const plusBtn = new ElementCreator(cartParams.counterBtnPlus);
+    counterWrapper.addInnerElement([minusBtn, counterInput, plusBtn]);
+
+    const itemPrice = new ElementCreator(cartParams.price);
+    itemPrice.setTextContent(`${price}$`);
+
+    const itemTotalPrice = new ElementCreator(cartParams.price);
+    itemTotalPrice.setTextContent(`${totalPrice}$`);
+
+    const deleteBtn = this.createDeleteBtn();
+
+    cartItem.addInnerElement([itemImage, itemName, counterWrapper, itemPrice, itemTotalPrice, deleteBtn]);
+    return cartItem;
+  }
+
+  private createDeleteBtn(): ElementCreator {
+    const deleteBtn = new ElementCreator(cartParams.buttonDelete);
+    deleteBtn.setAttribute('type', cartParams.buttonDelete.type);
+    const btnImage = new ElementCreator(cartParams.buttonDeleteImg);
+    btnImage.setAttribute('src', cartParams.buttonDeleteImg.src);
+    btnImage.setAttribute('alt', cartParams.buttonDeleteImg.alt);
+    deleteBtn.addInnerElement(btnImage);
+    return deleteBtn;
+  }
+
+  private assembleAside(): ElementCreator {
+    const cartAsideMenu = new ElementCreator(cartParams.asideMenu);
+    const asideHeading = new ElementCreator(cartParams.asideHeading);
+    const totalCost = this.createtotalCost('50', '0', '50');
+    cartAsideMenu.addInnerElement([asideHeading, this.createPromoCode(), totalCost]);
+    return cartAsideMenu;
+  }
+
+  private createPromoCode(): ElementCreator {
+    const promoWrapper = new ElementCreator(cartParams.promoWrapper);
+    const promoText = new ElementCreator(cartParams.promoText);
+    promoText.setAttribute('for', cartParams.promoText.for);
+
+    const promoInput = new ElementCreator(cartParams.promoInput);
+    promoInput.setAttribute('type', cartParams.promoInput.type);
+    promoInput.setAttribute('id', cartParams.promoInput.id);
+    promoInput.setAttribute('placeholder', cartParams.promoInput.placeholder);
+
+    const promoBtn = new ElementCreator(cartParams.promoBtn);
+    promoBtn.setAttribute('type', cartParams.promoBtn.type);
+
+    promoWrapper.addInnerElement([promoText, promoInput, promoBtn]);
+    return promoWrapper;
+  }
+
+  private createtotalCost(subtotal: string, discount: string, cost: string): ElementCreator {
+    const costWrapper = new ElementCreator(cartParams.costWrapper);
+
+    const subtotalWrapper = new ElementCreator(cartParams.subtotalWrapper);
+    const subtotalText = new ElementCreator(cartParams.subtotalText);
+    const subtotalNumber = new ElementCreator(cartParams.subtotalNumber);
+    subtotalNumber.setTextContent(`${subtotal}$`);
+    subtotalWrapper.addInnerElement([subtotalText, subtotalNumber]);
+
+    const discountWrapper = new ElementCreator(cartParams.discountWrapper);
+    const discountText = new ElementCreator(cartParams.discountText);
+    const discountNumber = new ElementCreator(cartParams.discountNumber);
+    discountNumber.setTextContent(`${discount}$`);
+    discountWrapper.addInnerElement([discountText, discountNumber]);
+
+    const totalWrapper = new ElementCreator(cartParams.totalWrapper);
+    const totalCostText = new ElementCreator(cartParams.totalCostText);
+    const totalCostNumber = new ElementCreator(cartParams.totalCostNumber);
+    totalCostNumber.setTextContent(`${cost}$`);
+    totalWrapper.addInnerElement([totalCostText, totalCostNumber]);
+
+    costWrapper.addInnerElement([subtotalWrapper, discountWrapper, totalWrapper]);
+    return costWrapper;
+  }
+
+  // private createCartItem() {
+  //   const itemImage = new ElementCreator(cartParams.itemsWrapper);
+  // }
 }
--- src/components/view/cart/cart-params.ts
+++ src/components/view/cart/cart-params.ts
@@ -3,10 +3,202 @@ const cartParams = {
     tag: 'section',
     cssClasses: ['cart'],
   },
+
   wrapper: {
     tag: 'div',
-    cssClasses: ['cart__wrapper', 'wrapper'],
-    textContent: 'CART IS HERE',
+    cssClasses: ['cart__wrapper'],
+  },
+
+  topWrapper: {
+    tag: 'div',
+    cssClasses: ['top__wrapper'],
+  },
+
+  heading: {
+    tag: 'h2',
+    cssClasses: ['cart__heading'],
+    textContent: 'Your Shopping Cart',
+  },
+
+  clearCartBtn: {
+    tag: 'button',
+    cssClasses: ['clear-cart__btn'],
+    textContent: 'Remove All Items',
+  },
+
+  clearCartBtnImg: {
+    tag: 'img',
+    cssClasses: ['clear-cart__btn__img'],
+    alt: 'clear-cart-button',
+    src: '../assets/img/delete.svg',
+  },
+
+  asideMenu: {
+    tag: 'aside',
+    cssClasses: ['aside__menu'],
+  },
+
+  itemsWrapper: {
+    tag: 'div',
+    cssClasses: ['cart-items__wrapper'],
+  },
+
+  cartItem: {
+    tag: 'div',
+    cssClasses: ['cart__item'],
+  },
+
+  img: {
+    tag: 'img',
+    cssClasses: ['cart-item_image'],
+  },
+
+  title: {
+    tag: 'div',
+    cssClasses: ['cart-item_title'],
+  },
+
+  info: {
+    tag: 'p',
+    cssClasses: ['cart-item_info'],
+  },
+
+  singer: {
+    tag: 'a',
+    cssClasses: ['cart-item_singer'],
+  },
+
+  priceWrapper: {
+    tag: 'div',
+    cssClasses: ['cart-item_prices'],
+  },
+
+  price: {
+    tag: 'div',
+    cssClasses: ['cart-item_price'],
+  },
+
+  counterWrapper: {
+    tag: 'div',
+    cssClasses: ['cart-item_counter-wrapper'],
+  },
+
+  counterBtnMinus: {
+    tag: 'button',
+    cssClasses: ['cart-item_counter-btn'],
+    textContent: '‐',
+  },
+
+  counterInput: {
+    tag: 'input',
+    type: 'number',
+    cssClasses: ['cart-item_counter-input'],
+  },
+
+  counterBtnPlus: {
+    tag: 'button',
+    cssClasses: ['cart-item_counter-btn'],
+    textContent: '+',
+  },
+
+  buttonDelete: {
+    tag: 'button',
+    cssClasses: ['delete-cart-item__btn'],
+    type: 'button',
+  },
+
+  buttonDeleteImg: {
+    tag: 'img',
+    cssClasses: ['delete-cart-item-btn__img'],
+    alt: 'delete-button',
+    src: '../assets/img/delete.svg',
+  },
+
+  asideHeading: {
+    tag: 'h3',
+    cssClasses: ['aside__heading'],
+    textContent: 'Order Summary',
+  },
+
+  promoWrapper: {
+    tag: 'form',
+    cssClasses: ['promo-code__wrapper'],
+  },
+
+  promoText: {
+    tag: 'label',
+    cssClasses: ['promo-code__text'],
+    textContent: 'Do you have a promo code?',
+    for: 'promocode',
+  },
+
+  promoInput: {
+    tag: 'input',
+    cssClasses: ['promo-code__input'],
+    placeholder: 'ENTER CODE HERE',
+    type: 'text',
+    id: 'promocode',
+  },
+
+  promoBtn: {
+    tag: 'button',
+    cssClasses: ['promo-code__btn'],
+    textContent: 'APPLY',
+    type: 'submit',
+  },
+
+  costWrapper: {
+    tag: 'div',
+    cssClasses: ['total-cost__wrapper'],
+  },
+
+  subtotalWrapper: {
+    tag: 'div',
+    cssClasses: ['subtotal__wrapper'],
+  },
+
+  subtotalText: {
+    tag: 'span',
+    cssClasses: ['subtotal__text'],
+    textContent: 'Subtotal',
+  },
+
+  subtotalNumber: {
+    tag: 'span',
+    cssClasses: ['subtotal__number'],
+  },
+
+  discountWrapper: {
+    tag: 'div',
+    cssClasses: ['discount__wrapper'],
+  },
+
+  discountText: {
+    tag: 'span',
+    cssClasses: ['discount__text'],
+    textContent: 'Promo discount',
+  },
+
+  discountNumber: {
+    tag: 'span',
+    cssClasses: ['discount__number'],
+  },
+
+  totalWrapper: {
+    tag: 'div',
+    cssClasses: ['total__wrapper'],
+  },
+
+  totalCostText: {
+    tag: 'span',
+    cssClasses: ['total-cost__text'],
+    textContent: 'TOTAL COST',
+  },
+
+  totalCostNumber: {
+    tag: 'span',
+    cssClasses: ['total-cost__number'],
+    // textContent: 'TOTAL COST',
   },
 };
 
